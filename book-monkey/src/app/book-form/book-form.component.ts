815b9e8446c519898d3d0828562d689678d8e454
ibrahim Kemler <ibram@gmx.de>
2022-01-15 21:35:37 +0100

    form kann gespeichert werden

--- book-monkey/src/app/book-form/book-form.component.ts
+++ book-monkey/src/app/book-form/book-form.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Output, EventEmitter, ViewChild } from '@angular/core';
 import { BookFactory } from '../shared/book-factory';
-import { Book } from '../shared/book';
-import { NgForm } from '@angular/forms';
+import { Book, Thumbnail } from '../shared/book';
+import { FormGroup, FormBuilder, NgForm, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'bm-book-form',
@@ -9,19 +9,71 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./book-form.component.css']
 })
 export class BookFormComponent implements OnInit {
-  book: Book = BookFactory.empty();
+  //book: Book = BookFactory.empty();
+  bookForm!: FormGroup;
 
   @Output() submitBook = new EventEmitter<Book>();
-  @ViewChild( 'bookForm', { static: true } ) bookForm?: NgForm;
+  //@ViewChild( 'bookForm', { static: true } ) bookForm?: NgForm;
 
-  constructor() { }
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.initForm();
+  }
+
+  private initForm() {
+    if ( this.bookForm ) { return; }
+
+    this.bookForm = this.fb.group({
+      title: ['', Validators.required],
+      subtitle: [''],
+      isbn: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(13)]],
+      description: [''],
+      authors: this.buildAuthorsArray(['']),
+      thumbnails: this.buildThumbnailsArray([{title: '', url: ''}]),
+      published: []
+    });
   }
 
   submitForm(): void {
-    this.submitBook.emit(this.book);
-    this.book = BookFactory.empty();
+    const formValue = this.bookForm?.value;
+
+    const authors = formValue.authors.filter( (author: string) => author );
+    const thumbnails = formValue.thumbnails.filter( (thumbnail: Thumbnail) => thumbnail.url );
+
+    const newBook: Book = {
+      ...formValue,
+      authors,
+      thumbnails
+    };
+
+    this.submitBook.emit( newBook );
     this.bookForm?.reset();
+  }
+
+  private buildAuthorsArray( values: string[] ): FormArray {
+    return this.fb.array( values, Validators.required );
+  }
+
+  private buildThumbnailsArray( values: Thumbnail[] ): FormArray {
+    return this.fb.array( values.map( t => this.fb.group( t ) ) );
+  }
+
+  get authors(): FormArray {
+    return this.bookForm?.get('authors') as FormArray;
+  }
+
+  get thumbnails(): FormArray {
+    return this.bookForm?.get('thumbnails') as FormArray;
+  }
+
+  addAuthorControl() {
+    this.authors.push( this.fb.control('') );
+  }
+
+  addThumbnailControl() {
+    this.thumbnails.push(
+      this.fb.group( { url:'', title:'' } )
+    );
   }
 }
